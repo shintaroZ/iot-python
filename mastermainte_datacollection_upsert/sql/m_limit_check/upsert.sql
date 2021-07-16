409d76de4db5d7fc3c220928042406504446b49e
shinz <bonjiki@hotmail.co.jp>
2021-07-16 16:59:11 +0900

    sql修正

--- mastermainte_datacollection_upsert/sql/m_limit_check/upsert.sql
+++ mastermainte_datacollection_upsert/sql/m_limit_check/upsert.sql
@@ -25,43 +25,44 @@ values (
     , %(version)d
 )
 ON DUPLICATE KEY UPDATE
-    LIMIT_CHECK_SEQ = if(LIMIT_CHECK_SEQ = values(LIMIT_CHECK_SEQ), values(LIMIT_CHECK_SEQ), LIMIT_CHECK_SEQ)
-    ,LIMIT_COUNT_TYPE = if(LIMIT_COUNT_TYPE = values(LIMIT_COUNT_TYPE), values(LIMIT_COUNT_TYPE), LIMIT_COUNT_TYPE)
-    ,LIMIT_COUNT = if(LIMIT_COUNT = values(LIMIT_COUNT), values(LIMIT_COUNT), LIMIT_COUNT)
-    ,LIMIT_COUNT_RESET_RANGE = if(LIMIT_COUNT_RESET_RANGE = values(LIMIT_COUNT_RESET_RANGE), values(LIMIT_COUNT_RESET_RANGE), LIMIT_COUNT_RESET_RANGE)
-    ,ACTION_RANGE = if(ACTION_RANGE = values(ACTION_RANGE), values(ACTION_RANGE), ACTION_RANGE)
-    ,NEXT_ACTION = if(NEXT_ACTION = values(NEXT_ACTION), values(NEXT_ACTION), NEXT_ACTION)
-    ,UPDATED_AT = 
+    UPDATED_AT = 
         case
             when LIMIT_CHECK_SEQ <> values(LIMIT_CHECK_SEQ) 
-             and LIMIT_COUNT_TYPE <> values(LIMIT_COUNT_TYPE)
-             and LIMIT_COUNT <> values(LIMIT_COUNT)
-             and LIMIT_COUNT_RESET_RANGE <> values(LIMIT_COUNT_RESET_RANGE)
-             and ACTION_RANGE <> values(ACTION_RANGE)
-             and NEXT_ACTION <> values(NEXT_ACTION)
+             or LIMIT_COUNT_TYPE <> values(LIMIT_COUNT_TYPE)
+             or LIMIT_COUNT <> values(LIMIT_COUNT)
+             or LIMIT_COUNT_RESET_RANGE <> values(LIMIT_COUNT_RESET_RANGE)
+             or ACTION_RANGE <> values(ACTION_RANGE)
+             or NEXT_ACTION <> values(NEXT_ACTION)
             then '%(updatedAt)s'
             else UPDATED_AT
         end
-    ,UPDATED_USER = 
+    , UPDATED_USER = 
         case
             when LIMIT_CHECK_SEQ <> values(LIMIT_CHECK_SEQ) 
-             and LIMIT_COUNT_TYPE <> values(LIMIT_COUNT_TYPE)
-             and LIMIT_COUNT <> values(LIMIT_COUNT)
-             and LIMIT_COUNT_RESET_RANGE <> values(LIMIT_COUNT_RESET_RANGE)
-             and ACTION_RANGE <> values(ACTION_RANGE)
-             and NEXT_ACTION <> values(NEXT_ACTION)
+             or LIMIT_COUNT_TYPE <> values(LIMIT_COUNT_TYPE)
+             or LIMIT_COUNT <> values(LIMIT_COUNT)
+             or LIMIT_COUNT_RESET_RANGE <> values(LIMIT_COUNT_RESET_RANGE)
+             or ACTION_RANGE <> values(ACTION_RANGE)
+             or NEXT_ACTION <> values(NEXT_ACTION)
             then values(UPDATED_USER)
             else UPDATED_USER
         end
-    ,VERSION = 
+    , VERSION = 
         case
             when LIMIT_CHECK_SEQ <> values(LIMIT_CHECK_SEQ) 
-             and LIMIT_COUNT_TYPE <> values(LIMIT_COUNT_TYPE)
-             and LIMIT_COUNT <> values(LIMIT_COUNT)
-             and LIMIT_COUNT_RESET_RANGE <> values(LIMIT_COUNT_RESET_RANGE)
-             and ACTION_RANGE <> values(ACTION_RANGE)
-             and NEXT_ACTION <> values(NEXT_ACTION)
+             or LIMIT_COUNT_TYPE <> values(LIMIT_COUNT_TYPE)
+             or LIMIT_COUNT <> values(LIMIT_COUNT)
+             or LIMIT_COUNT_RESET_RANGE <> values(LIMIT_COUNT_RESET_RANGE)
+             or ACTION_RANGE <> values(ACTION_RANGE)
+             or NEXT_ACTION <> values(NEXT_ACTION)
             then values(VERSION)
             else VERSION
         end
+    , LIMIT_CHECK_SEQ = if(LIMIT_CHECK_SEQ = values(LIMIT_CHECK_SEQ), LIMIT_CHECK_SEQ, values(LIMIT_CHECK_SEQ) )
+    , LIMIT_COUNT_TYPE = if(LIMIT_COUNT_TYPE = values(LIMIT_COUNT_TYPE), LIMIT_COUNT_TYPE, values(LIMIT_COUNT_TYPE) )
+    , LIMIT_COUNT = if(LIMIT_COUNT = values(LIMIT_COUNT), LIMIT_COUNT, values(LIMIT_COUNT) )
+    , LIMIT_COUNT_RESET_RANGE = if(LIMIT_COUNT_RESET_RANGE = values(LIMIT_COUNT_RESET_RANGE), LIMIT_COUNT_RESET_RANGE, values(LIMIT_COUNT_RESET_RANGE) )
+    , ACTION_RANGE = if(ACTION_RANGE = values(ACTION_RANGE), ACTION_RANGE, values(ACTION_RANGE) )
+    , NEXT_ACTION = if(NEXT_ACTION = values(NEXT_ACTION), NEXT_ACTION, values(NEXT_ACTION) )
+
 ;
--- mastermainte_datacollection_upsert/sql/m_link_flg/upsert.sql
+++ mastermainte_datacollection_upsert/sql/m_link_flg/upsert.sql
@@ -17,27 +17,27 @@ values (
     , %(version)d
 )
 ON DUPLICATE KEY UPDATE
-    SAVING_FLG = if(SAVING_FLG = values(SAVING_FLG), values(SAVING_FLG), SAVING_FLG)
-    ,LIMIT_CHECK_FLG = if(LIMIT_CHECK_FLG = values(LIMIT_CHECK_FLG), values(LIMIT_CHECK_FLG), LIMIT_CHECK_FLG)
-    ,UPDATED_AT = 
+    UPDATED_AT = 
         case
             when SAVING_FLG <> values(SAVING_FLG) 
-             and LIMIT_CHECK_FLG <> values(LIMIT_CHECK_FLG)
+              or LIMIT_CHECK_FLG <> values(LIMIT_CHECK_FLG)
             then '%(updatedAt)s'
             else UPDATED_AT
         end
-    ,UPDATED_USER = 
+    , UPDATED_USER = 
         case
             when SAVING_FLG <> values(SAVING_FLG) 
-             and LIMIT_CHECK_FLG <> values(LIMIT_CHECK_FLG)
+              or LIMIT_CHECK_FLG <> values(LIMIT_CHECK_FLG)
             then values(UPDATED_USER)
             else UPDATED_USER
         end
-    ,VERSION = 
+    , VERSION = 
         case
             when SAVING_FLG <> values(SAVING_FLG) 
-             and LIMIT_CHECK_FLG <> values(LIMIT_CHECK_FLG)
+              or LIMIT_CHECK_FLG <> values(LIMIT_CHECK_FLG)
             then values(VERSION)
             else VERSION
         end
+    , SAVING_FLG = if(SAVING_FLG = values(SAVING_FLG), SAVING_FLG, values(SAVING_FLG) )
+    , LIMIT_CHECK_FLG = if(LIMIT_CHECK_FLG = values(LIMIT_CHECK_FLG), LIMIT_CHECK_FLG, values(LIMIT_CHECK_FLG) )
 ;
