dff15811451a648c4f7b28787fb00969cd530f49
shinz <shintaro_otoi@icloud.com>
2022-04-05 14:11:55 +0900

    mqcommon add

--- layer/python/mqCommon.py
+++ layer/python/mqCommon.py
@@ -7,22 +7,8 @@ import pika
 import ssl
 from enum import IntEnum
 import json
-from _ast import Try
 
-# ----------------- 上り要求 -----------------
-# 0 : スコアデータ
-# 1 : 異常音
-# 2 : エッジ再起動
-# 3 : 再起動失敗
-# 4 : 現在音
-# --------------------------------------------
-class MqUpEnum(IntEnum):
-    Last_Score = 0
-    New_Error = 1
-    Restart_Edge = 2
-    Restart_Edge_Failed = 3
-    Transfer_Current_Sound = 4
-    
+
 # -------------------------
 # MQ接続共通クラス
 # -------------------------
@@ -39,23 +25,29 @@ class mqCommon:
     
     # ルーティングキー / キュー名
     ROUTING_QUEUE = []
-    ROUTING_QUEUE.append( {"Last_Score" : "Last_Score"})
-    ROUTING_QUEUE.append( {"New_Error" : "New_Error"})
-    ROUTING_QUEUE.append( {"Restart_Edge" : "Restart_Edge"})
-    ROUTING_QUEUE.append( {"Restart_Edge_Failed" : "Restart_Edge_Failed"})
-    ROUTING_QUEUE.append( {"Transfer_Current_Sound" : "Transfer_Current_Sound"})
+    ROUTING_QUEUE.append({"Last_Score": "Last_Score"})
+    ROUTING_QUEUE.append({"New_Error": "New_Error"})
+    ROUTING_QUEUE.append({"Restart_Edge": "Restart_Edge"})
+    ROUTING_QUEUE.append({"Restart_Edge_Failed": "Restart_Edge_Failed"})
+    ROUTING_QUEUE.append({"Transfer_Current_Sound": "Transfer_Current_Sound"})
 
     # TENANT_ID
     TENANT_ID = "tenantId"
+
     # --------------------------------------------------
     # プロパティ
     # --------------------------------------------------
     def getConnect(self):
-        global MQ_CONNECT
-        return MQ_CONNECT
+        return self.MQ_CONNECT
+
     def getLogger(self):
-        global LOGGER
-        return LOGGER
+        return self.LOGGER
+
+    def getExchangesUpName(self):
+        return self.EXCHANGES_UP_NAME
+
+    def getExchangesDwName(self):
+        return self.EXCHANGES_DW_NAME
 
     # --------------------------------------------------
     # コンストラクタ パラメータを元にRDS接続を行う
@@ -65,94 +57,113 @@ class mqCommon:
     # port(int)           : 接続先ポート番号
     # user(str)           : ユーザ
     # password(str)       : パスワード
-    # dbName(str)         : 接続先インスタンス
-    # connectTimeout(int) : 接続タイムアウト(s)
-    # autoCommit(bool)    : 自動コミット
     # --------------------------------------------------
-    def __init__(self, logger=None, host="localhost", port=5671, user="hoge", password="hoge" ,dbName="hoge"
-                 ,connectTimeout=3, autoCommit=False):
-        global LOGGER, MQ_CONNECT, CHANNEL
-        LOGGER = logger
-        MQ_CONNECT = None
+    def __init__(self, logger=None, host="localhost", port=5671, user="hoge", password="hoge" ):
+        # global LOGGER, MQ_CONNECT, CHANNEL
+        self.LOGGER = logger
+        # self.MQ_CONNECT = None
 
         try:
-            # AmazonMQ接続先URL作成（amqps://{ユーザ}:{パスワード}@{接続先エンドポイント}:{ポート番号}）
-            connectUrl = "amqps://%s:%s@%s:%d" % (user, password, host, port)
-            LOGGER.debug(connectUrl)
-        
             # SSL Context for TLS configuration of Amazon MQ for RabbitMQ
             ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
             ssl_context.set_ciphers('ECDHE+AESGCM:!ECDSA')
             
+            # AmazonMQ接続先URL作成（amqps://{ユーザ}:{パスワード}@{接続先エンドポイント}:{ポート番号}）
+            connectUrl = "amqps://%s:%s@%s:%d" % (user, password, host, port)
+            self.LOGGER.debug(connectUrl)
             connect_param = pika.URLParameters(connectUrl)
-            # connect_param = pika.ConnectionParameters(connectUrl, heartbeat=10)
-            # connect_param = pika.ConnectionParameters(connectUrl, connection_attempts=3)
             connect_param.ssl_options = pika.SSLOptions(context=ssl_context)
+            # connect_param = pika.ConnectionParameters(connectUrl, connection_attempts=5)
             
             # AmazonMQコネクション作成
-            LOGGER.info("---- MQ Connection Start")
-            MQ_CONNECT = pika.BlockingConnection(connect_param)
-            LOGGER.info("---- MQ Connection Successfull")
+            self.LOGGER.info("---- MQ Connection Start")
+            self.MQ_CONNECT = pika.BlockingConnection(connect_param)
+            self.LOGGER.info("---- MQ Connection Successfull")
             
             # チャンネル作成
-            CHANNEL = MQ_CONNECT.channel()
+            self.CHANNEL = self.MQ_CONNECT.channel()
         except Exception  as e:
-            LOGGER.error("---- MQ Connection Failed")
+            self.LOGGER.error("---- MQ Connection Failed")
             raise(e)
 
     # --------------------------------------------------
-    # デストラクタ コネsクションをクローズ
+    # デストラクタ コネクションをクローズ
     # --------------------------------------------------
     def __del__(self):
-        global MQ_CONNECT
-        if MQ_CONNECT is not None:
-            MQ_CONNECT.close()
-
+        if (self.MQ_CONNECT is not None) and (self.MQ_CONNECT.is_closed == False):
+            self.MQ_CONNECT.close()
 
     # --------------------------------------------------
-    # キューのメッセージ数を取得
+    # キューからメッセージを取得
     # queueName(str)  : キュー名
+    # idArray(array)  : 取得対象のTENANT_IDを配列で指定
     # --------------------------------------------------
-    def getQueueCount(self, queueName):
-        
-        # キュー毎のメッセージの件数出力
-        queueCount = CHANNEL.queue_declare(queue=queueName,
-                                            durable=True,  
-                                            exclusive=False,
-                                            auto_delete=False).method.message_count
-        LOGGER.info("%s max count : %d" % (queueName, queueCount))
-        
-        return queueCount
-    
-    # --------------------------------------------------
-    # キューのメッセージ数を取得
-    # queueName(str)  : キュー名
-    # --------------------------------------------------
-    def getQueueMessage(self, queueName):
+    def getQueueMessage(self, queueName, idArray = None):
         records = []
         
         # キューからメッセージ取得
-        for method_frame, properties, body in CHANNEL.consume(queue=queueName, inactivity_timeout=0.5):
+        for method_frame, properties, body in self.CHANNEL.consume(queue=queueName, inactivity_timeout=0.5):
     
+            isAck = False
             # inactivity_timeout(秒)を超えるとNone返却するので終了
             if method_frame is None:
                 break
             else:
-                print ("***** %s" % body.decode('utf-8'))
-                # 文字列データをJson形式にパース
-                jsonDict = json.loads(body)
-                records.append(jsonDict)
-                # ACKを返すことでキュー消化
-                # channel.basic_ack(delivery_tag=method_frame.delivery_tag)
+                try:
+                    # 文字列データをJson形式にパース
+                    jsonDict = json.loads(body)
+                    
+                    # 取得判定
+                    isAck = self.__isQueueContains(jsonDict, idArray)
+
+                except json.JSONDecodeError as ex:
+                    # Json形式でない場合はスキップ
+                    self.LOGGER.warn("フォーマット不正の為、スキップします。:%s" % body)
+                    jsonDict = body
+                
+                if isAck:
+                    records.append(jsonDict)
+                    # ACKを返すことでキュー消化
+                    self.CHANNEL.basic_ack(delivery_tag=method_frame.delivery_tag)
         
-        LOGGER.info("%s result : %s" % (queueName, records))
+        self.LOGGER.info("%s result : %s" % (queueName, records))
         return records
     
-    def isQueue(self, jsonDict, tenantId):
-        try:
-            return (tenantId == jsonDict[TENANT_ID])
-            
-        except Exception  as e:
-            LOGGER.error("---- MQ Connection Failed")
-            raise(e)
+    # --------------------------------------------------
+    # エクスチェンジへメッセージを送信
+    # exchangeName(str)  : エクスチェンジ名
+    # routingKey(str)    : ルーティングキー
+    # bodyMessage(str)   : メッセージボディ部
+    # exchangeType(str)  : エクスチェンジタイプ
+    # --------------------------------------------------
+    def publishExchange(self, exchangeName, routingKey, bodyMessage="{}", exchangeType="topic"):
         
+        # ExchangeTypeの生成
+        self.CHANNEL.exchange_declare(exchange=exchangeName,
+                                 exchange_type=exchangeType,
+                                 durable=True)
+                            
+        # メッセージ送信
+        self.CHANNEL.basic_publish(exchange=exchangeName,
+                             routing_key=routingKey,
+                             body=bodyMessage,
+                             properties=pika.BasicProperties(
+                                 delivery_mode=2,
+                                 headers={"content_type": "application/json"}
+                                ))
+        
+    
+    # キュー内のTENANT_IDに顧客対象のIDが含まれるかを判定
+    def __isQueueContains(self, jsonDict, idArray):
+        isResult = False
+        try:
+            if idArray is None:
+                isResult=  True
+            elif(jsonDict[self.TENANT_ID] in idArray):
+                isResult = True
+        except KeyError  as e:
+            # TENANT_IDが存在しない
+            self.LOGGER.warn("TENANT_IDが存在しないため、スキップします。:%s" % jsonDict)
+        
+        return isResult
+0
--- layer/python/mqCommonTest.py
+++ layer/python/mqCommonTest.py
@@ -8,7 +8,7 @@ import rdsCommon as rds
 import initCommon
 import mqCommon as mq
 import configparser
-
+import sys
 
 DB_HOST = "mysql.cpp9recuwclr.ap-northeast-1.rds.amazonaws.com"
 DB_PORT = 3306
@@ -20,26 +20,131 @@ MQ_HOST = "eg-iot-nlb-a11512e8be0beed3.elb.ap-northeast-1.amazonaws.com"
 MQ_PORT = 5671
 MQ_USER = "monone_dev"
 MQ_PASSWORD = "VTwJMnmyqiE2"
+MQ_UT_EXCANGE = "ut.exchange"
+MQ_UT_ROUTINGKEY = "ut.queue"
+MQ_UT_QUEUE = "ut.queue"
+
+MQ_CONNECT = None
+LOGGER = None
+
 
 def get_query(query_file_path):
     with open(query_file_path, 'r', encoding='utf-8') as f:
         query = f.read()
     return query
 
+
 def createEvent(query_file_path):
     f = open(query_file_path, 'r')
     return json.load(f)
 
+# -------------------------------------
+# MQ接続共通クラスのテストクラス
+# -------------------------------------
 class MqCommonTest(unittest.TestCase):
 
-    def test_mqCommon_001(self):
-        print("------------ %s start------------" % "test_mqCommon_001" )
-        logger = initCommon.getLogger("DEBUG")
-        mqIns = mq.mqCommon(logger,MQ_HOST,MQ_PORT,MQ_USER,MQ_PASSWORD)
+    # テスト開始時に1回だけ呼び出される
+    # クラスメソッドとして定義する
+    @classmethod
+    def setUpClass(cls):
+        global MQ_CONNECT, LOGGER
+        LOGGER = initCommon.getLogger("INFO")
+        # MQ接続（リトライ付き）
+        for num in range(1, 10):
+            try:
+                MQ_CONNECT = mq.mqCommon(LOGGER, MQ_HOST, MQ_PORT, MQ_USER, MQ_PASSWORD)
+                break
+            except Exception as ex:
+                print("***** mq connect retry [%d]" % num)
         
-        resultCount = mqIns.getQueueCount("New_Error")
-        resultMessage = mqIns.getQueueMessage("New_Error")
+            
+    @classmethod
+    def tearDownClass(cls):
+        global MQ_CONNECT
+        # MQ切断
+        print ("***** mq close")
+        del MQ_CONNECT
         
-        print("------------ %s end------------" % "test_mqCommon_001x" )
-        del mqIns
+    # テストメソッドを実行するたびに呼ばれる
+    def setUp(self):
+        pass
+ 
+    # テストメソッドの実行が終わるたびに呼ばれる
+    def tearDown(self):
+        pass
+    
+    # ------------------------------------------------------
+    # キューの中身を全て取得してキューが空になること。
+    # ------------------------------------------------------
+    def test_getQueueMessage_001(self):
+        print("------------ %s start------------" % sys._getframe().f_code.co_name)
+
+        # キューの中身をクリア
+        MQ_CONNECT.getQueueMessage(MQ_UT_QUEUE)
+       
+        # テストメッセージ送信
+        MQ_CONNECT.publishExchange(MQ_UT_EXCANGE, MQ_UT_ROUTINGKEY, get_query("test/mq/newError001.json"))
+        MQ_CONNECT.publishExchange(MQ_UT_EXCANGE, MQ_UT_ROUTINGKEY, get_query("test/mq/newError002.json"))
         
+        # 全メッセージ取得
+        resultArray = MQ_CONNECT.getQueueMessage(MQ_UT_QUEUE)
+        for result in resultArray:
+            print(result)
+            
+        
+    # ------------------------------------------------------
+    # TENANT_ID:2のみキューを取得すること。
+    # ------------------------------------------------------
+    def test_getQueueMessage_002(self):
+        print("------------ %s start------------" % sys._getframe().f_code.co_name)
+
+        # キューの中身をクリア
+        MQ_CONNECT.getQueueMessage(MQ_UT_QUEUE)
+       
+        # テストメッセージ送信
+        MQ_CONNECT.publishExchange(MQ_UT_EXCANGE, MQ_UT_ROUTINGKEY, get_query("test/mq/newError001.json"))
+        MQ_CONNECT.publishExchange(MQ_UT_EXCANGE, MQ_UT_ROUTINGKEY, get_query("test/mq/newError002.json"))
+        
+        # 一部メッセージだけ取得
+        resultArray = MQ_CONNECT.getQueueMessage(MQ_UT_QUEUE, ["2"])
+        for result in resultArray:
+            print(result)
+
+       
+    # ------------------------------------------------------
+    # TENANT_IDが存在しない場合は無視されること。
+    # ------------------------------------------------------
+    def test_getQueueMessage_003(self):
+        print("------------ %s start------------" % sys._getframe().f_code.co_name)
+
+        # キューの中身をクリア
+        MQ_CONNECT.getQueueMessage(MQ_UT_QUEUE)
+       
+        # テストメッセージ送信
+        MQ_CONNECT.publishExchange(MQ_UT_EXCANGE, MQ_UT_ROUTINGKEY, get_query("test/mq/newError001.json"))
+        MQ_CONNECT.publishExchange(MQ_UT_EXCANGE, MQ_UT_ROUTINGKEY, get_query("test/mq/newError002.json"))
+        MQ_CONNECT.publishExchange(MQ_UT_EXCANGE, MQ_UT_ROUTINGKEY, get_query("test/mq/newError003.json"))
+        
+        # 一部メッセージだけ取得
+        resultArray = MQ_CONNECT.getQueueMessage(MQ_UT_QUEUE, ["2"])
+        for result in resultArray:
+            print(result)
+            
+    # ------------------------------------------------------
+    # Json形式でない場合は無視されること。
+    # ------------------------------------------------------
+    def test_getQueueMessage_004(self):
+        print("------------ %s start------------" % sys._getframe().f_code.co_name)
+
+        # キューの中身をクリア
+        MQ_CONNECT.getQueueMessage(MQ_UT_QUEUE)
+       
+        # テストメッセージ送信
+        MQ_CONNECT.publishExchange(MQ_UT_EXCANGE, MQ_UT_ROUTINGKEY, get_query("test/mq/newError001.json"))
+        MQ_CONNECT.publishExchange(MQ_UT_EXCANGE, MQ_UT_ROUTINGKEY, get_query("test/mq/newError002.json"))
+        MQ_CONNECT.publishExchange(MQ_UT_EXCANGE, MQ_UT_ROUTINGKEY, "平文テスト123")
+        
+        # 一部メッセージだけ取得
+        resultArray = MQ_CONNECT.getQueueMessage(MQ_UT_QUEUE, ["2"])
+        for result in resultArray:
+            print(result)
