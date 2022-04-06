546690702d8a078e09aa2bed543bdd9f40e71ebd
shinz <shintaro_otoi@icloud.com>
2022-04-06 15:35:39 +0900

    収集区分マスタ参照を追加

---
+++ mastermainte_collectiontype_get/lambda_function.py
@@ -0,0 +1,153 @@
+import json
+import sys
+import datetime
+import configparser
+import initCommon  # カスタムレイヤー
+import rdsCommon  # カスタムレイヤー
+
+
+# global
+LOGGER = None
+CONNECT = None
+LOG_LEVEL = "INFO"
+DB_HOST = "localhost"
+DB_PORT = 3306
+DB_USER = "hoge"
+DB_PASSWORD = "hoge"
+DB_NAME = "hoge"
+DB_CONNECT_TIMEOUT = 3
+RETRY_MAX_COUNT = 3
+RETRY_INTERVAL = 500
+
+# setter
+def setLogger(logger):
+    global LOGGER
+    LOGGER = logger
+
+
+def setLogLevel(loglevel):
+    global LOG_LEVEL
+    LOG_LEVEL = loglevel
+
+
+def setDbHost(dbHost):
+    global DB_HOST
+    DB_HOST = dbHost
+
+
+def setDbPort(dbPort):
+    global DB_PORT
+    DB_PORT = int(dbPort)
+
+
+def setDbUser(dbUser):
+    global DB_USER
+    DB_USER = dbUser
+
+
+def setDbPassword(dbPassword):
+    global DB_PASSWORD
+    DB_PASSWORD = dbPassword
+
+
+def setDbName(dbName):
+    global DB_NAME
+    DB_NAME = dbName
+
+
+def setDbConnectTimeout(dbConnectTimeout):
+    global DB_CONNECT_TIMEOUT
+    DB_CONNECT_TIMEOUT = int(dbConnectTimeout)
+
+
+def setRetryMaxCount(retryMaxCount):
+    global RETRY_MAX_COUNT
+    RETRY_MAX_COUNT = int(retryMaxCount)
+
+
+def setRetryInterval(retryInterval):
+    global RETRY_INTERVAL
+    RETRY_INTERVAL = int(retryInterval)
+
+
+# --------------------------------------------------
+# 設定ファイル読み込み
+# --------------------------------------------------
+def initConfig(clientName):
+    try:
+        # 設定ファイル読み込み
+        result = initCommon.getS3Object(clientName, "config.ini")
+
+        # ConfigParserへパース
+        config_ini = configparser.ConfigParser()
+        config_ini.read_string(result)
+
+        setLogLevel(config_ini['logger setting']['loglevel'])
+        setDbHost(config_ini['rds setting']['host'])
+        setDbPort(config_ini['rds setting']['port'])
+        setDbUser(config_ini['rds setting']['user'])
+        setDbPassword(config_ini['rds setting']['password'])
+        setDbName(config_ini['rds setting']['db'])
+        setDbConnectTimeout(config_ini['rds setting']['connect_timeout'])
+        setRetryMaxCount(config_ini['rds setting']['retryMaxcount'])
+        setRetryInterval(config_ini['rds setting']['retryinterval'])
+    except Exception as e:
+        print ('設定ファイルの読み込みに失敗しました。')
+        raise(e)
+
+
+# --------------------------------------------------
+# 起動パラメータチェック
+# --------------------------------------------------
+def isArgument(event):
+
+    # トークン取得
+    token = event["idToken"]
+    
+    # グループ名
+    try:
+        groupList = initCommon.getPayLoadKey(token, "cognito:groups")
+    
+        # 顧客名がグループ名に含まれること
+        if (event["clientName"] not in groupList):
+            raise Exception("clientNameがグループに属していません。clientName:%s groupName:%s" % (event["clientName"], ",".join(groupList) ))
+    except Exception as ex:
+        raise Exception("Authentication Error. [%s]" %  ex)
+        
+
+# --------------------------------------------------
+# 戻り値整形
+# --------------------------------------------------
+def convertResult(result):
+
+    # Dict→str形式に変換して返却
+    return json.dumps(result, ensure_ascii=False, default=initCommon.json_serial)
+
+
+#####################
+# main
+#####################
+def lambda_handler(event, context):
+
+    # 初期処理
+    initConfig(event["clientName"])
+    setLogger(initCommon.getLogger(LOG_LEVEL))
+
+    LOGGER.info('マスタメンテナンス機能_収集区分マスタ参照開始 : %s' % event)
+
+    # 入力チェック
+    isArgument(event)
+
+    # RDSコネクション作成
+    rds = rdsCommon.rdsCommon(LOGGER, DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME, DB_CONNECT_TIMEOUT)
+
+    # マスタselect
+    result = rds.fetchall(initCommon.getQuery("sql/m_collection_type/findAll.sql"))
+
+    # 戻り値整形
+    reReult = convertResult(result)
+
+    # close
+    del rds
+
+    return reReult
---
+++ mastermainte_collectiontype_get/sql/m_collection_type/findAll.sql
@@ -0,0 +1,9 @@
+select
+    EDGE_TYPE as edgeType
+    , COLLECTION_TYPE as collectionType
+    , COLLECTION_TYPE_NAME as collectionTypeName
+    , DISP_ORDER as dispOrder
+    , CREATED_AT as createdAt
+from
+    M_COLLECTION_TYPE
+;
