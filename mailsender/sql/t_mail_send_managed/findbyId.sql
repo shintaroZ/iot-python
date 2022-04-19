3019c4bde0bcd49ec3c48ad1cf68683692ee5c5d
shinz <shintaro_otoi@icloud.com>
2022-04-19 11:54:34 +0900

    mailSend phase3

--- mailsender/sql/t_mail_send_managed/findbyId.sql
+++ mailsender/sql/t_mail_send_managed/findbyId.sql
@@ -3,7 +3,12 @@ select
     , tmsm.DETECTION_DATETIME as detectionDateTime
     , tmsm.LIMIT_SUB_NO as limitSubNo
     , tmsm.MAIL_SEND_SEQ as mailSendSeq
-    , tpt.SENSOR_VALUE as sensorValue
+    , case
+       when tpt.SENSOR_VALUE is null
+       then ts.DETECTION_FLAG
+       else tpt.SENSOR_VALUE
+      end as sensorValue
+    , ts.DETECTION_DATETIME
     , mdc.DEVICE_ID as deviceId
     , mdc.SENSOR_ID as sensorId
     , mdc.SENSOR_NAME as sensorName
@@ -26,14 +31,17 @@ from
     inner join M_DATA_COLLECTION mdc
         on tmsm.DATA_COLLECTION_SEQ = mdc.DATA_COLLECTION_SEQ
         and mdc.DELETE_FLG = 0
-    inner join T_PUBLIC_TIMESERIES tpt
-        on tmsm.DETECTION_DATETIME = tpt.RECEIVED_DATETIME
-        and tmsm.DATA_COLLECTION_SEQ = tpt.DATA_COLLECTION_SEQ
     inner join M_LIMIT_CHECK mlc
         on mlc.DATA_COLLECTION_SEQ = mdc.DATA_COLLECTION_SEQ
     inner join M_LIMIT ml
         on ml.DATA_COLLECTION_SEQ = mlc.DATA_COLLECTION_SEQ
         and ml.LIMIT_SUB_NO = tmsm.LIMIT_SUB_NO
+    left outer join T_PUBLIC_TIMESERIES tpt
+        on tpt.DATA_COLLECTION_SEQ = tmsm.DATA_COLLECTION_SEQ
+        and tpt.RECEIVED_DATETIME = tmsm.DETECTION_DATETIME
+    left outer join T_SCORE ts
+        on ts.DATA_COLLECTION_SEQ = tmsm.DATA_COLLECTION_SEQ
+        and ts.DETECTION_DATETIME = tmsm.DETECTION_DATETIME
 where
 	tmsm.MAIL_SEND_SEQ = %(mailSendSeq)d
 and tmsm.SEND_STATUS = 0
